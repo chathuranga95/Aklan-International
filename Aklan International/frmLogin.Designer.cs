b10c6b192d0b3ba713822018e8dd619071317c15
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-07-01 14:45:34 +0530

    c

--- Aklan International/frmLogin.Designer.cs
+++ Aklan International/frmLogin.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(665, 308);
+            this.ClientSize = new System.Drawing.Size(504, 318);
             this.Controls.Add(this.btnLogin);
             this.Name = "frmLogin";
             this.Text = "Login";
