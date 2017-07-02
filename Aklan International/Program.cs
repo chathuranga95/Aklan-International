9031e87ca9b6dddab785e91622750a2698d93d58
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2017-07-02 13:25:58 +0530

    db ok

--- Aklan International/Program.cs
+++ Aklan International/Program.cs
@@ -17,7 +17,7 @@ namespace Aklan_International
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
-            Application.Run(new CreateNewOrder.FrmCreateOrder());
+            
         }
     }
 }
