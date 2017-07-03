049e8d3d2a7a20223775a40d532dc836194983fb
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-07-03 16:02:55 +0530

    worker window

---
+++ Aklan International/frmWorkerWindow.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Aklan_International
+{
+    public partial class frmWorkerWindow : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+
+        public frmWorkerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+           
+            cmd = new SqlCommand("select * from dtLogin where username = '" + txtUserName.Text.Trim() + "'", conn);
+            try
+            {
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    if (reader["password"].ToString().Trim() == txtPassword.Text.Trim())
+                    {
+
+                        MessageBox.Show("Login success...");
+                        //frmMain obj = new frmMain(reader["userName"].ToString());
+                        //obj.Show();
+                        this.Hide();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login failed...");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Login failed...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
