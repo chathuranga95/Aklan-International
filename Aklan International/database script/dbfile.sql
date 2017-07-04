a4821b3f50db5b7d76424ad448a2c4ff6cd2ff71
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-07-04 23:38:37 +0530

    database script file updated

--- Aklan International/database script/dbfile.sql
+++ Aklan International/database script/dbfile.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `dta001`
+--
+
+DROP TABLE IF EXISTS `dta001`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dta001` (
+  `index` int(11) NOT NULL,
+  `date` varchar(20) DEFAULT NULL,
+  `matType` varchar(45) DEFAULT NULL,
+  `Qty` int(11) DEFAULT NULL,
+  `finished` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dta001`
+--
+
+LOCK TABLES `dta001` WRITE;
+/*!40000 ALTER TABLE `dta001` DISABLE KEYS */;
+INSERT INTO `dta001` VALUES (1,'7/4/2017','Sheet',2,'no');
+/*!40000 ALTER TABLE `dta001` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `dtlogin`
 --
 
@@ -39,6 +66,32 @@ LOCK TABLES `dtlogin` WRITE;
 INSERT INTO `dtlogin` VALUES ('a001','1234','shehan'),('s001','1234','kamal'),('sm001','1234','saman'),('w001','1234','nimal');
 /*!40000 ALTER TABLE `dtlogin` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `dtmaterial`
+--
+
+DROP TABLE IF EXISTS `dtmaterial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dtmaterial` (
+  `index` int(11) NOT NULL,
+  `empID` varchar(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `material` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dtmaterial`
+--
+
+LOCK TABLES `dtmaterial` WRITE;
+/*!40000 ALTER TABLE `dtmaterial` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dtmaterial` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -49,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-03 19:33:13
+-- Dump completed on 2017-07-04 23:21:44
