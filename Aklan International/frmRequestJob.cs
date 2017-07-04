feade9d35c62edbd130c4c87e3e5b917fd55e496
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-07-04 23:10:46 +0530

    Requesting job logic added, database updated

--- Aklan International/frmRequestJob.cs
+++ Aklan International/frmRequestJob.cs
@@ -7,22 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Aklan_International
 {
     public partial class frmRequestJob : Form
     {
+        MySqlCommand cmd;
+        MySqlConnection conn;
+        MySqlDataReader reader;
         Form frmobj;
+        String empID;
         public frmRequestJob()
         {
             InitializeComponent();
         }
 
-        public frmRequestJob(String workerName, Form frmobj)
+        public frmRequestJob(String workerName, Form frmobj, String empID)
         {
             InitializeComponent();
             lblWorkerName.Text = workerName;
             this.frmobj = frmobj;
+            this.empID = empID;
         }
         private void btnExit_Click(object sender, EventArgs e)
         {
@@ -32,7 +38,51 @@ namespace Aklan_International
 
         private void frmRequestJob_Load(object sender, EventArgs e)
         {
+            conn = new MySqlConnection("Server=localhost;Database=dbcore;Uid=root;Pwd=1234");
+        }
 
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                cmd = new MySqlCommand("SELECT * FROM information_schema.tables WHERE table_schema = 'dbcore' AND table_name = 'dt" + empID + "'", conn);
+                reader = cmd.ExecuteReader();
+                int index = 0;
+                if (!reader.Read())
+                {
+                    cmd = new MySqlCommand("CREATE TABLE `dbcore`.`dt"+empID.ToString()+ "` ( `index` INT NOT NULL,`date` VARCHAR(20) NULL, `matType` VARCHAR(45) NULL,`Qty` INT NULL, `finished` VARCHAR(3) NULL, PRIMARY KEY(`index`))", conn);
+                    reader.Close();
+                    cmd.ExecuteNonQuery();
+                    index = 1;
+                }
+                else
+                {
+                    //retrieve index
+                    index++;
+                }
+
+                conn.Close();
+                conn.Open();
+                Dictionary<String, String> mydic = new Dictionary<string, string>();
+                mydic.Add("Cutting", "Sheet");
+                mydic.Add("Clip Cutting", "Cut strip");
+                mydic.Add("Folding", "Clip cut strip");
+                mydic.Add("Rimming", "Folded strip");
+                string matType;
+                mydic.TryGetValue(cmbMachineType.Text.Trim(),out matType);
+                cmd = new MySqlCommand("INSERT INTO `dbcore`.`dt"+empID.ToString()+"` (`index`, `date`, `matType`, `Qty`, `finished`) VALUES ('"+index.ToString()+"', '"+DateTime.Today.Date.ToShortDateString()+"', '"+matType+"', '"+spnQty.Value.ToString()+"', 'no')",conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch 
+            {
+                //MessageBox.Show(ee.ToString());
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
