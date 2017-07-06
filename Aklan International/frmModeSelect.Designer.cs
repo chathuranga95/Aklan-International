7b131f38e8fb84ede607f9c1dc5f56ae636b5db5
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-07-06 20:58:45 +0530

    mode selecting form added. logic added

---
+++ Aklan International/frmModeSelect.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Aklan_International
+{
+    partial class frmModeSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(156, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Admin Mode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(156, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Worker Mode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmModeSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 239);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmModeSelect";
+            this.Text = "Select Mode";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Aklan International/frmModeSelect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aklan_International
+{
+    public partial class frmModeSelect : Form
+    {
+        public frmModeSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //admin window to work on
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmWorkerWindow obj = new frmWorkerWindow();
+            obj.Show();
+            this.Close();
+        }
+    }
+}
