d705d67094bad332efb57a7662b6873f98b5910f
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-07-09 10:34:51 +0530

    admin window designer modified

--- Aklan International/frmAdminWindow.Designer.cs
+++ Aklan International/frmAdminWindow.Designer.cs
@@ -29,20 +29,28 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnManageOrder = new System.Windows.Forms.Button();
+            this.btnViewOrders = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.btnViewOrders = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnManageStock = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnManageSales = new System.Windows.Forms.Button();
+            this.btnViewSales = new System.Windows.Forms.Button();
+            this.btnManageWorkers = new System.Windows.Forms.Button();
+            this.btnViewWorkers = new System.Windows.Forms.Button();
+            this.btnCreateOrder = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnCreateOrder);
+            this.groupBox1.Controls.Add(this.btnManageOrder);
             this.groupBox1.Controls.Add(this.btnViewOrders);
             this.groupBox1.Location = new System.Drawing.Point(28, 12);
             this.groupBox1.Name = "groupBox1";
@@ -51,34 +59,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Orders";
             // 
-            // groupBox2
+            // btnManageOrder
             // 
-            this.groupBox2.Location = new System.Drawing.Point(28, 268);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(577, 122);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Sales";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.button3);
-            this.groupBox3.Controls.Add(this.button2);
-            this.groupBox3.Location = new System.Drawing.Point(28, 140);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(577, 122);
-            this.groupBox3.TabIndex = 1;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Stock";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Location = new System.Drawing.Point(28, 396);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(577, 122);
-            this.groupBox4.TabIndex = 2;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Work";
+            this.btnManageOrder.Location = new System.Drawing.Point(344, 33);
+            this.btnManageOrder.Name = "btnManageOrder";
+            this.btnManageOrder.Size = new System.Drawing.Size(85, 37);
+            this.btnManageOrder.TabIndex = 1;
+            this.btnManageOrder.Text = "Manage Orders";
+            this.btnManageOrder.UseVisualStyleBackColor = true;
             // 
             // btnViewOrders
             // 
@@ -89,14 +77,36 @@
             this.btnViewOrders.Text = "View Orders";
             this.btnViewOrders.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // groupBox2
             // 
-            this.button1.Location = new System.Drawing.Point(166, 33);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Manage Orders";
-            this.button1.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.btnManageSales);
+            this.groupBox2.Controls.Add(this.btnViewSales);
+            this.groupBox2.Location = new System.Drawing.Point(28, 268);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(577, 122);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sales";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnManageStock);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(28, 140);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(577, 122);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Stock";
+            // 
+            // btnManageStock
+            // 
+            this.btnManageStock.Location = new System.Drawing.Point(166, 41);
+            this.btnManageStock.Name = "btnManageStock";
+            this.btnManageStock.Size = new System.Drawing.Size(85, 38);
+            this.btnManageStock.TabIndex = 0;
+            this.btnManageStock.Text = "Manage Stock";
+            this.btnManageStock.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -107,14 +117,61 @@
             this.button2.Text = "View Stock";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // groupBox4
             // 
-            this.button3.Location = new System.Drawing.Point(166, 41);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(85, 38);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "Manage Stock";
-            this.button3.UseVisualStyleBackColor = true;
+            this.groupBox4.Controls.Add(this.btnManageWorkers);
+            this.groupBox4.Controls.Add(this.btnViewWorkers);
+            this.groupBox4.Location = new System.Drawing.Point(28, 396);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(577, 122);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Work";
+            // 
+            // btnManageSales
+            // 
+            this.btnManageSales.Location = new System.Drawing.Point(166, 39);
+            this.btnManageSales.Name = "btnManageSales";
+            this.btnManageSales.Size = new System.Drawing.Size(85, 38);
+            this.btnManageSales.TabIndex = 1;
+            this.btnManageSales.Text = "Manage Sales orders";
+            this.btnManageSales.UseVisualStyleBackColor = true;
+            // 
+            // btnViewSales
+            // 
+            this.btnViewSales.Location = new System.Drawing.Point(16, 39);
+            this.btnViewSales.Name = "btnViewSales";
+            this.btnViewSales.Size = new System.Drawing.Size(85, 38);
+            this.btnViewSales.TabIndex = 2;
+            this.btnViewSales.Text = "View Sales Orders";
+            this.btnViewSales.UseVisualStyleBackColor = true;
+            // 
+            // btnManageWorkers
+            // 
+            this.btnManageWorkers.Location = new System.Drawing.Point(166, 47);
+            this.btnManageWorkers.Name = "btnManageWorkers";
+            this.btnManageWorkers.Size = new System.Drawing.Size(85, 38);
+            this.btnManageWorkers.TabIndex = 3;
+            this.btnManageWorkers.Text = "Manage Workers";
+            this.btnManageWorkers.UseVisualStyleBackColor = true;
+            // 
+            // btnViewWorkers
+            // 
+            this.btnViewWorkers.Location = new System.Drawing.Point(16, 47);
+            this.btnViewWorkers.Name = "btnViewWorkers";
+            this.btnViewWorkers.Size = new System.Drawing.Size(85, 38);
+            this.btnViewWorkers.TabIndex = 4;
+            this.btnViewWorkers.Text = "View Workers";
+            this.btnViewWorkers.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateOrder
+            // 
+            this.btnCreateOrder.Location = new System.Drawing.Point(183, 33);
+            this.btnCreateOrder.Name = "btnCreateOrder";
+            this.btnCreateOrder.Size = new System.Drawing.Size(85, 37);
+            this.btnCreateOrder.TabIndex = 1;
+            this.btnCreateOrder.Text = "Create Order";
+            this.btnCreateOrder.UseVisualStyleBackColor = true;
             // 
             // frmAdminWindow
             // 
@@ -128,7 +185,9 @@
             this.Name = "frmAdminWindow";
             this.Text = "Admin";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -139,9 +198,14 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnManageOrder;
         private System.Windows.Forms.Button btnViewOrders;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnManageStock;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnManageSales;
+        private System.Windows.Forms.Button btnViewSales;
+        private System.Windows.Forms.Button btnManageWorkers;
+        private System.Windows.Forms.Button btnViewWorkers;
+        private System.Windows.Forms.Button btnCreateOrder;
     }
 }
