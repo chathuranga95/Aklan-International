29cc5ea4059e6390b748923aeeb9475a310cb90f
Mahesh Suranga <mahesh.15@cse.mrt.ac.lk>
2017-07-14 19:04:20 +0530

    over manufcturing alert class

---
+++ Aklan International/OverManAlert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aklan_International
+{
+    class OverManAlert
+    {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        MySqlDataReader reader;
+
+        bool finished;
+
+        public OverManAlert()
+        {
+            conn = new MySqlConnection("server=localhost;user id=root;database=dbcore;pwd=1234;allowuservariables=True");
+            conn.Open();
+        }
+        public bool CheckOrderList()
+        {
+            cmd = new MySqlCommand("SELECT * from dtcustomer_orders where finished = 'no'", conn);
+            reader = cmd.ExecuteReader();
+        }
+    }
+}
