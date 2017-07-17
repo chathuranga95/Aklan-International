12b59fe3f831f1461f19d67f08dcd21b743fb082
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2017-07-17 09:42:23 +0530

    Change Password Form Designed

--- Aklan International/Properties/Resources.Designer.cs
+++ Aklan International/Properties/Resources.Designer.cs
@@ -333,6 +333,16 @@ namespace Aklan_International.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap password {
+            get {
+                object obj = ResourceManager.GetObject("password", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Payroll {
             get {
                 object obj = ResourceManager.GetObject("Payroll", resourceCulture);
--- Aklan International/frmChangePassword.Designer.cs
+++ Aklan International/frmChangePassword.Designer.cs
@@ -36,12 +36,14 @@
             this.label4 = new System.Windows.Forms.Label();
             this.btnChange = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 32);
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(145, 39);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 1;
@@ -49,24 +51,25 @@
             // 
             // txtOldPass
             // 
-            this.txtOldPass.Location = new System.Drawing.Point(144, 32);
+            this.txtOldPass.Location = new System.Drawing.Point(257, 39);
             this.txtOldPass.Name = "txtOldPass";
             this.txtOldPass.PasswordChar = '*';
-            this.txtOldPass.Size = new System.Drawing.Size(100, 20);
+            this.txtOldPass.Size = new System.Drawing.Size(188, 20);
             this.txtOldPass.TabIndex = 4;
             // 
             // txtNewPass
             // 
-            this.txtNewPass.Location = new System.Drawing.Point(144, 72);
+            this.txtNewPass.Location = new System.Drawing.Point(257, 79);
             this.txtNewPass.Name = "txtNewPass";
             this.txtNewPass.PasswordChar = '*';
-            this.txtNewPass.Size = new System.Drawing.Size(100, 20);
+            this.txtNewPass.Size = new System.Drawing.Size(188, 20);
             this.txtNewPass.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 72);
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(145, 79);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 5;
@@ -74,16 +77,17 @@
             // 
             // txtConfirmPass
             // 
-            this.txtConfirmPass.Location = new System.Drawing.Point(144, 108);
+            this.txtConfirmPass.Location = new System.Drawing.Point(257, 115);
             this.txtConfirmPass.Name = "txtConfirmPass";
             this.txtConfirmPass.PasswordChar = '*';
-            this.txtConfirmPass.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmPass.Size = new System.Drawing.Size(188, 20);
             this.txtConfirmPass.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 108);
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(145, 115);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 13);
             this.label4.TabIndex = 7;
@@ -91,29 +95,43 @@
             // 
             // btnChange
             // 
-            this.btnChange.Location = new System.Drawing.Point(303, 164);
+            this.btnChange.BackColor = System.Drawing.Color.White;
+            this.btnChange.Location = new System.Drawing.Point(370, 174);
             this.btnChange.Name = "btnChange";
             this.btnChange.Size = new System.Drawing.Size(75, 23);
             this.btnChange.TabIndex = 9;
             this.btnChange.Text = "Change";
-            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.UseVisualStyleBackColor = false;
             this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(184, 164);
+            this.btnCancel.BackColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(257, 174);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 10;
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImage = global::Aklan_International.Properties.Resources.password;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(-25, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 145);
+            this.panel1.TabIndex = 11;
             // 
             // frmChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 220);
+            this.BackgroundImage = global::Aklan_International.Properties.Resources.Admin_Window_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(464, 221);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnChange);
             this.Controls.Add(this.txtConfirmPass);
@@ -122,6 +140,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtOldPass);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
             this.Name = "frmChangePassword";
             this.Text = "Change Password";
             this.Load += new System.EventHandler(this.frmChangePassword_Load);
@@ -139,5 +159,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnChange;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Panel panel1;
     }
 }
